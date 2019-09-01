f5ef02f3be3c12ab20cc613cbe2dd745a7cd70a5
Halil güler <h.guler3717@gmail.com>
2019-09-01 11:37:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function calculatesFarePrice(start,destinationPoint){
     return 0 ;
   }
   else if  (distance > 400 && distance <=2000){
-    return (distance-400)*2;
+    return (distance-400)*0.02;
   }
   else if (distance >2000 && distance <2500){
     return 25;
